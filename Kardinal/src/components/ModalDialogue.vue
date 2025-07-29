4cde79e8b8cf56c1ba60290e491b79ded2a015a1
AndreeOak <andreesoul@gmail.com>
2025-07-29 20:13:29 -0300

    Focusing the modal when opening

--- Kardinal/src/components/ModalDialogue.vue
+++ Kardinal/src/components/ModalDialogue.vue
@@ -1,20 +1,31 @@
 <script lang="ts" setup>
+import { ref, watch } from 'vue';
 
-defineProps<{
+
+const props = defineProps<{
   isOpen: boolean
 }>()
 
 const emit = defineEmits<{(e:'close'):void}>()
+
+const titleInput = ref<HTMLInputElement | null>(null)
+
+watch(() => props.isOpen, (isOpen) => {
+  if (isOpen) {
+    setTimeout(() => titleInput.value?.focus(), 0)
+  }
+})
+
 </script>
 
 
 <template>
 
-  <div class="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center" role="dialog" aria-model="true" v-if="isOpen" @keydown.esc="emit('close')">
+  <div class="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center" role="dialog" aria-modal="true" v-if="isOpen" @keydown.esc="emit('close')">
     <div class="bg-white p-5 rounded max-w-md w-full">
       <h2 class="text-xl font-bold mb-4">Add New Card</h2>
       <input type="text" placeholder="Card Title"
-      aria-label="Card Title" class ="w-full p-2 mb-4 border rounded" />
+      aria-label="Card Title" class ="w-full p-2 mb-4 border rounded" ref="titleInput"/>
 
       <textarea class ="w-full p-2 mb-4 border rounded" placeholder="description" aria-label="Card Description"/>
       <div class="flex justify-end gap-2">

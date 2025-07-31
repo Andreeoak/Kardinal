f518f9566b00752595f2ca129b4d2faf86e6afc2
AndreeOak <andreesoul@gmail.com>
2025-07-30 22:16:49 -0300

    Header component

---
+++ Kardinal/src/components/SignatureHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex justify-between items-center h-14 px-6 bg-indigo-600 shadow-md">
+    <div class="text-3xl font-bold tracking-tight text-white drop-shadow-sm italic" id="title">
+      Kardinal
+    </div>
+    <div class="text-lg font-medium tracking-tight text-indigo-100 italic">
+      @Andreeoak
+    </div>
+  </div>
+</template>
